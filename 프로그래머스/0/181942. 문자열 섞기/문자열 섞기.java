f63bf4f2e5344ebd7f1d73b8f7ed6028b55437f3
thzthix <95135403+thzthix@users.noreply.github.com>
2025-05-24 14:31:55 +0900

    [level 0] Title: 문자열 섞기, Time: 15.43 ms, Memory: 91.8 MB -BaekjoonHub

--- 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
+++ 프로그래머스/0/181942. 문자열 섞기/문자열 섞기.java
@@ -1,20 +1,9 @@
 class Solution {
     public String solution(String str1, String str2) {
-        int fullLength = str1.length() + str2.length();
         String answer = "";
-        int str1index = 0;
-        int str2index = 0;
-        for(int i =0;i<fullLength;i++){
-
-            if(i%2 == 0){
-                answer += str1.charAt(str1index);
-                str1index++;
-                
-            }
-            else{
-                answer+=str2.charAt(str2index);
-                str2index++;
-            }
+        for(int i=0;i<str1.length();i++){
+            answer+=str1.charAt(i);
+            answer+=str2.charAt(i);
         }
         return answer;
     }
