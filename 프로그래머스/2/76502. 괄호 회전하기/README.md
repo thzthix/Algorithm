eb265864010dc2bb182d7f595a7bb02c85053dd5
thzthix <95135403+thzthix@users.noreply.github.com>
2025-06-29 10:48:47 +0900

    [level 2] Title: 괄호 회전하기, Time: 98.67 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/README.md
+++ 프로그래머스/2/76502. 괄호 회전하기/README.md
@@ -1,6 +1,6 @@
 # [level 2] 괄호 회전하기 - 76502 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/76502?language=python3) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/76502) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 29일 10:48:17
+2025년 06월 29일 10:48:46
 
 ### 문제 설명
 
--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.py
@@ -8,19 +8,21 @@ def solution(s):
     #입력 배열 개수만큼 반복
     for j in range(0, len(brackets)):
         for i in range (0,len(brackets)):
+            #성공 여부 초기화
+            is_success = True
             #스택에 "{"나 "(" 이면
             current = brackets[i]
-            is_success = True
             if current in ["{","(","["]:
                 stack.append(current)
             #스택에 집어넣는다.
             else:
                 if not stack:
-                    is_success = False
-                    break
+                    brackets = change_brackets(brackets)
+                    continue
                 #스택에서 "}" 나 ")" 이면
                 #만약 stack에 아무것도 없으면 skip
                 head = stack[-1]
+                print(stack)
                 #스택에서 "}" 일 때 "{"이고 스택에서 ")"일 때 ")"면
                 if (current == "}" and head == "{") or (current == ")" and head == "(") or (current==']' and head == '['):
                 #pop
@@ -28,7 +30,7 @@ def solution(s):
                     stack.pop()
                 #아니면 skip
                 #스택이 비었으면 count++
-        if is_success and not stack:
+        if not stack:
             count+=1
         stack = []
     #인덱스 슬라이싱으로 회전
