ad1f394bde283cbe3f78c4305050956cdb3bb241
thzthix <95135403+thzthix@users.noreply.github.com>
2025-06-30 21:37:21 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 64.83 ms, Memory: 27.2 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -3,6 +3,6 @@ def solution(arr):
     #stack에 없으면 넣는다.
     stack = []
     for n in arr:
-        if n not in stack or stack[-1] != n:
+        if not stack or stack[-1] != n:
             stack.append(n)
     return stack
