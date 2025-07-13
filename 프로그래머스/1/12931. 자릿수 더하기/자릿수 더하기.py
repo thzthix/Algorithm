3fe436e6cb0067504b5009bf38d77c515bda0389
thzthix <95135403+thzthix@users.noreply.github.com>
2025-07-13 14:54:14 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.03 ms, Memory: 9.38 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.py
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.py
@@ -1,4 +1,3 @@
 from functools import reduce
 def solution(n):
-    number_list = list(str(n))
-    return reduce( lambda acc, value: acc+int(value),number_list, 0)
+    return sum(int(digit) for digit in str(n))
