e6a4e159cb78faf4abd7bcea6d0ace8453885d3a
thzthix <95135403+thzthix@users.noreply.github.com>
2025-07-13 15:15:25 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.03 ms, Memory: 9.41 MB -BaekjoonHub

--- 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.py
+++ 프로그래머스/1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.py
@@ -1,4 +1,2 @@
 def solution(n):
-    numbers_to_sort = [digit for digit in str(n)]
-    numbers_to_sort.sort(reverse=True)
-    return int("".join(n for n in numbers_to_sort))
+    return int(("".join(sorted(str(n), reverse=True))))
