2bded88eb527bd27742726002d91fdb6cf9e64d4
thzthix <95135403+thzthix@users.noreply.github.com>
2025-07-13 22:40:39 +0900

    [level 2] Title: 프로세스, Time: 0.92 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -1,19 +1,18 @@
 
 from collections import deque
-import math
 def solution(priorities, location):
     q = deque()
     for i in range(0,len(priorities)):
         q.append({"location":i, "value": priorities[i]})
     count = 0
-    while priorities:
+    while q:
         c = q.popleft()
-        max_p = max(priorities)
-        if(c["value"] == max_p):
-            priorities.remove(max_p)
+        if any(process["value"] > c["value"] for process in q):
+            q.append(c)
+        else:
             count+=1
             if(c["location"] == location):
                 return count
-        else:
-            q.append(c)
+        
+            
  
