18c7927716bd67cd4385bfa9d685935b33698b1d
thzthix <95135403+thzthix@users.noreply.github.com>
2025-07-15 23:44:55 +0900

    [level 2] Title: H-Index, Time: 0.18 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -5,17 +5,12 @@ def solution(citations):
     #가장 큰 값부터 찾는다.
     #그 값 이상인게 h이상, 아닌게 h이하라면 h-index
     citations.sort(reverse = True)
-    max_value = 0
-    current_value  = citations[0] 
-    last_value = 0
-    while(current_value > 0):
-        high_array = [c for c in citations if c >= current_value]
-        if len(high_array) >= current_value and current_value != last_value:
-            return current_value
-        last_value = current_value
-        current_value -=1
+    length = len(citations)
+    for i in range(length):
+        if citations[i] <= i:
+            return i
         
-    return 0
+    return length
         
         
         
