8138b91a57984081cba9e2e0ce0a103eb68cdeff
thzthix <95135403+thzthix@users.noreply.github.com>
2025-07-15 21:28:03 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.01 ms, Memory: 9.11 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -1,14 +1,8 @@
 def solution(n, arr1, arr2):
     answer = []
-    for i in range(len(arr1)):
-        deconde_str = ""
-        bin_arr1 = bin(arr1[i])[2:].zfill(n)
-        bin_arr2 = bin(arr2[i])[2:].zfill(n)
-        decode_str = ""
-        for j in range(n):
-            if bin_arr1[j] == "0" and bin_arr2[j] == "0":
-                decode_str += " "
-            else:
-                decode_str += "#"
-        answer.append(decode_str)
+    for i,j in zip(arr1, arr2):
+        bin_result = bin(i | j)[2:].zfill(n)
+        bin_result  = bin_result.replace("0", " ")
+        bin_result  = bin_result.replace("1", "#")
+        answer.append(bin_result)
     return answer
