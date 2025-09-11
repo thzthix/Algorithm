c9c3286bf3f54056f6d0d183e62efd4435fff2f3
Seoha Son <95135403+thzthix@users.noreply.github.com>
2025-09-11 19:29:40 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,5 +1,23 @@
+def quick_sort(a):
+    if len(a) <= 1: 
+        return a
+    left = []
+    right = []
+    pivots = []
+    piv = a[0]
+    for i in a:
+        if i < piv:
+            left.append(i)
+        elif i > piv:
+            right.append(i)
+        else:
+            pivots.append(i)
+    return quick_sort(left) + pivots + quick_sort(right)
 def solution(array, commands):
-    answer = []
-    for command in commands:
-        answer.append(sorted(array[(command[0]-1):command[1]])[command[2]-1])
-    return answer
+    answers = []
+    for com in commands:
+        sorted_arr = quick_sort(array[com[0]-1:com[1]])
+        answers.append(sorted_arr[com[2]-1])
+    return answers
+        
+    
