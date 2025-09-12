fdfc32e156bbbed84314ef2775480a7cdf8d306a
Seoha Son <95135403+thzthix@users.noreply.github.com>
2025-09-12 14:53:47 +0900

    [level 1] Title: K번째수, Time: 0.03 ms, Memory: 9.09 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,23 +1,32 @@
-def quick_sort(a):
-    if len(a) <= 1: 
-        return a
-    left = []
-    right = []
-    pivots = []
-    piv = a[0]
-    for i in a:
-        if i < piv:
-            left.append(i)
-        elif i > piv:
-            right.append(i)
-        else:
-            pivots.append(i)
-    return quick_sort(left) + pivots + quick_sort(right)
-def solution(array, commands):
-    answers = []
-    for com in commands:
-        sorted_arr = quick_sort(array[com[0]-1:com[1]])
-        answers.append(sorted_arr[com[2]-1])
-    return answers
-        
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        L = arr[:mid]
+        R = arr[mid:]
+        merge_sort(L)
+        merge_sort(R)
+        i = j = k = 0
+        while i<len(L) and j < len(R):
+            if L[i] < R[j]:
+                arr[k] = L[i]
+                i+=1
+            else:
+                arr[k] = R[j]
+                j+=1
+            k+=1
+        while i<len(L):
+            arr[k] = L[i]
+            i+=1
+            k+=1
+        while j <len(R):
+            arr[k] = R[j]
+            j+=1
+            k+=1
     
+def solution(array, commands):
+    answer = []
+    for com in commands:
+        temp_arr = array[com[0]-1:com[1]]
+        merge_sort(temp_arr)
+        answer.append(temp_arr[com[2]-1])
+    return answer
