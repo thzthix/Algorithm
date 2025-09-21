c099d008205ae952ab6e67a7c0b62096d7f96f5d
Seoha Son <95135403+thzthix@users.noreply.github.com>
2025-09-21 16:35:48 +0900

    [Silver III] Title: N과 M (2), Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,14 @@
+N,M = map(int,input().split())
+ans = []
+def back():
+    if len(ans) == M:
+        print(" ".join(map(str,ans)))
+        return
+    for i in range(1,N+1):
+        if i not in ans:
+            if len(ans) == 0 or i > ans[-1]:
+                ans.append(i)
+                back()
+                ans.pop()
+            
+back()
