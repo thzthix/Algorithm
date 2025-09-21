4dabd558a5ec162bb96d1dbd84b9f3201831f405
Seoha Son <95135403+thzthix@users.noreply.github.com>
2025-09-21 16:58:54 +0900

    [Silver III] Title: N과 M (3), Time: 1124 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,14 @@
+import sys
+sys.setrecursionlimit(10**6)
+N,M = map(int, input().split())
+ans=[]
+def back():
+    if len(ans) == M:
+        print(" ".join(map(str, ans)))
+        return
+    for i in range(1,N+1):
+        ans.append(i)
+        back()
+        ans.pop()
+
+back()
