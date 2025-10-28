cf4c469fb7d2be6948e92b18ffcea53b106bcf26
Seoha Son <95135403+thzthix@users.noreply.github.com>
2025-10-28 19:41:28 +0900

    Update DFS와 BFS.py

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,37 +1,39 @@
-
 from collections import deque
 
 
-def DFS(g,v,visited):
+def DFS(g,v,visited, result):
     visited[v] = True
-    visited_nodes_DFS.append(v)
+    result.append(v)
     for i in g[v]:
         if not visited[i]:
-            DFS(g,i,visited)
-def BFS(start_node, g):
+            DFS(g,i,visited, result)
+    return result
+
+def BFS(start_node, g, result):
     queue = deque([start_node])
     visited = [False] * (N+1)
     visited[start_node] = True
-    visited_nodes = [start_node]
+    result.append(start_node)
     while queue:
         curr_node = queue.popleft()
         for i in g[curr_node]:
             if not visited[i]:           
                 visited[i] = True
-                visited_nodes.append(i)
+                result.append(i)
                 queue.append(i)
-    return " ".join(map(str, visited_nodes))
+    return result
+
 N,M,V = map(int,input().split())
 
-graph = {i: [] for i in range(1, N+1)}
+graph = [[] for i in range(N+1)]
 visited_DFS = [False] * (N+1)
-visited_nodes_DFS = [] 
+result_DFS = []
+result_BFS = []
 for i in range(M):
     a,b = map(int,input().split())
     graph[a].append(b)
     graph[b].append(a)
-for i in range(1,N+1):
-    graph[i].sort()
-DFS(graph,V, visited_DFS)
-print(" ".join(map(str, visited_nodes_DFS)))
-print(BFS(V, graph))
+for adj_list in graph:
+    adj_list.sort()
+print(" ".join(map(str,DFS(graph,V, visited_DFS , result_DFS ))))
+print(" ".join(map(str, BFS(V,graph, result_BFS))))
