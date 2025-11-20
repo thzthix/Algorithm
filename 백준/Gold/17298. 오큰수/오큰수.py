8717955366fe418fb5731c4ee822191495b3fc44
Seoha Son <95135403+thzthix@users.noreply.github.com>
2025-11-20 16:55:36 +0900

    [Gold IV] Title: 오큰수, Time: 824 ms, Memory: 183880 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.py
+++ 백준/Gold/17298. 오큰수/오큰수.py
@@ -3,16 +3,12 @@ arr = list(map(int, input().split()))
 answer = [-1] * n
 stack = []
 
-for i in range(n-1, -1, -1):
-    # 현재 수보다 작거나 같은거 다 뺌
-    while stack and stack[-1] <= arr[i]:
-        stack.pop()
+for i in range(n):
+    # arr[i] 오큰수로 가질 애들 
+    while stack and arr[stack[-1]] < arr[i]:
+        idx = stack.pop()  
+        answer[idx] = arr[i]  
     
-    # 스택에 남은 게 있으면 그게 오큰수
-    if stack:
-        answer[i] = stack[-1]
-    
-    # 현재 값을 스택에 추가
-    stack.append(arr[i])
+    stack.append(i)  
 
 print(' '.join(map(str, answer)))
