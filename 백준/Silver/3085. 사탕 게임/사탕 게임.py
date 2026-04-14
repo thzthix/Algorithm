edf534d4a80fdce2d36e14778262157e6fb4af0b
Seoha Son <95135403+thzthix@users.noreply.github.com>
2026-04-14 18:53:47 +0900

    [Silver II] Title: 사탕 게임, Time: 96 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/3085. 사탕 게임/사탕 게임.py
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.py
@@ -1,7 +1,7 @@
 N = int(input())
 board = [list(input()) for _ in range(N)]
 answer = 1
-def search(R,C):
+def check_row(R):
     global answer
     cnt = 1
     for j in range (1, N):
@@ -10,6 +10,8 @@ def search(R,C):
             answer = max(answer, cnt)
         else:
             cnt = 1
+def check_column(C):
+    global answer
     cnt = 1
     for i in range(1, N):
         if board[i-1][C] == board[i][C]:
@@ -22,13 +24,15 @@ for i in range(N):
     for j in range(N):
         if j + 1 < N:
             board[i][j], board[i][j+1] = board[i][j+1], board[i][j]
-            search(i,j)
-            search(i,j+1)
+            check_row(i)
+            check_column(j)
+            check_column(j+1)
             board[i][j+1], board[i][j] = board[i][j], board[i][j+1]
         if i+1 < N:
             board[i][j], board[i+1][j] = board[i+1][j], board[i][j]
-            search(i,j)
-            search(i+1,j)
+            check_column(j)
+            check_row(i)
+            check_row(i+1)
             board[i+1][j], board[i][j] = board[i][j], board[i+1][j]
             
 print(answer)
