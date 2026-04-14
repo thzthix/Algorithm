5e6ce5ee1a871a66a71e35f30050acd521c745bc
Seoha Son <95135403+thzthix@users.noreply.github.com>
2026-04-14 18:46:43 +0900

    [Silver II] Title: 사탕 게임, Time: 124 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/3085. 사탕 게임/사탕 게임.py
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.py
@@ -1,35 +1,34 @@
 N = int(input())
 board = [list(input()) for _ in range(N)]
 answer = 1
-def search():
+def search(R,C):
     global answer
-    for i in range(N):
-        cnt = 1
-        for j in range (1, N):
-            if board[i][j-1] == board[i][j]:
-                cnt+=1
-                answer = max(answer, cnt)
-            else:
-                cnt = 1
-    
-    for j in range(N):
-        cnt = 1
-        for i in range(1, N):
-            if board[i-1][j] == board[i][j]:
-                cnt +=1
-                answer = max(answer, cnt)
-            else:
-                cnt = 1
+    cnt = 1
+    for j in range (1, N):
+        if board[R][j-1] == board[R][j]:
+            cnt+=1
+            answer = max(answer, cnt)
+        else:
+            cnt = 1
+    cnt = 1
+    for i in range(1, N):
+        if board[i-1][C] == board[i][C]:
+            cnt +=1
+            answer = max(answer, cnt)
+        else:
+            cnt = 1
     
 for i in range(N):
     for j in range(N):
         if j + 1 < N:
             board[i][j], board[i][j+1] = board[i][j+1], board[i][j]
-            search()
+            search(i,j)
+            search(i,j+1)
             board[i][j+1], board[i][j] = board[i][j], board[i][j+1]
         if i+1 < N:
             board[i][j], board[i+1][j] = board[i+1][j], board[i][j]
-            search()
+            search(i,j)
+            search(i+1,j)
             board[i+1][j], board[i][j] = board[i][j], board[i+1][j]
             
 print(answer)
