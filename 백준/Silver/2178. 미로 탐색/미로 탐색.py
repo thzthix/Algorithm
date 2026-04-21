2db88bdb76da3dbcf06735e0618dc66612b644ed
Seoha Son <95135403+thzthix@users.noreply.github.com>
2026-04-21 18:11:21 +0900

    [Silver I] Title: 미로 탐색, Time: 76 ms, Memory: 35092 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,30 +1,30 @@
 from collections import deque
-def BFS(end_x, end_y, graph):
-    dx = [-1,1,0,0]
-    dy = [0,0,-1,1]
-   
-    queue = deque([(1,1,1)])
-    visited = set([(1, 1)])
-    while queue:
-        x,y, path = queue.popleft()
-        if x == end_x and y == end_y:
-            return path
-        for i in range (4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 1<= nx <=end_x  and 1<= ny<=end_y  and graph[nx][ny] == 1:
-                if (nx, ny) not in visited:
-                    visited.add((nx,ny))
-                    queue.append((nx, ny, path + 1))
-            
-            
-    return -1
+
+dy = (0,1,0,-1)
+dx = (-1,0,1,0)
+
 N,M = map(int, input().split())
-graph = [ [0] * (M+1) for i in range(N+1)]
-for i in range(1,N+1):
-    row =  input().strip()
-    for j in range(1, M+1):
-        graph[i][j] = int(row[j-1])
-print(BFS(N,M,graph))
 
+board = [input() for _ in range(N)]
+chk = [[False]*M for _ in range(N)]
 
+dq = deque()
+
+dq.append((0,0,1))
+chk[0][0] = True
+
+def is_valid_choord(y,x):
+    return 0<=y<N and 0<=x<M
+
+while len(dq) > 0:
+    y,x,d = dq.popleft()
+    if y == N-1 and x == M-1:
+        print(d)
+        break
+    for k in range(4):
+        ny = y + dy[k]
+        nx = x + dx[k]
+        nd = d+1
+        if is_valid_choord(ny, nx) and board[ny][nx] == '1' and not chk[ny][nx]:
+            chk[ny][nx] = True
+            dq.append((ny, nx, nd))
